9a9cb7f44ea41b4efc5a49cc902283018fb46496
Donald Purnhagen <dpurnhagen@gmail.com>
2019-06-05 20:05:13 -0400

    Use switch to cycle brightness.

--- rotary-encoder-ring.ino
+++ rotary-encoder-ring.ino
@@ -3,9 +3,14 @@
 /* BEGIN - LED Ring Variables */
 #define LED_DATA_PIN 5
 #define LED_NUMBER 8
-#define LED_BRIGHTNESS 240
+#define LED_BRIGHTNESS_INC 16
+#define LED_BRIGHTNESS_MAX 239 
+#define LED_BRIGHTNESS_MIN 63 
 #define LED_TYPE WS2812B
 #define LED_COLOR_MODEL GRB
+// Initial brightness.
+byte iBrightness = 127;
+//byte iHue = HUE_ORANGE;
 CRGB leds[LED_NUMBER];
 
 /* BEGIN – Encoder Variables */
@@ -14,25 +19,25 @@ CRGB leds[LED_NUMBER];
 #define ENCODER_SWITCH_PIN 7
 #define ENCODER_POS_MIN 1
 #define ENCODER_POS_MAX 20
-
-const double ENCODER_POS_PER_LED = ((double)ENCODER_POS_MAX / LED_NUMBER);
+//const double ENCODER_POS_PER_LED = ((double)ENCODER_POS_MAX / LED_NUMBER);
 int iEncoderPos = 1;
 int iEncoderPosLast = 1;
-byte iHue = HUE_ORANGE;
 int iVal;
 int iValLast;
 boolean bCW;
 
 void doSetShow() {
-    int iLastLed = iEncoderPos / ENCODER_POS_PER_LED;
+    byte iLastLed = map(iEncoderPos, ENCODER_POS_MIN, ENCODER_POS_MAX, 0, LED_NUMBER); //iEncoderPos / ENCODER_POS_PER_LED;
+    Serial.print("Last LED: ");
+    Serial.println(iLastLed);
     for (int i = 0; i < LED_NUMBER; i++) {
       byte iV = 0;
-      byte iLED = (LED_NUMBER - ENCODER_POS_MIN - i);
       if (i < iLastLed) {
-        iV = (i * i + LED_BRIGHTNESS / 4);
+        iV = iBrightness; //(i * i + iBrightness / 4);
       }
 //      leds[iLED] = CHSV(iHue, 255, iV);
-      leds[iLED] = CHSV((i * 32), 255, iV);
+      // Evenly divide the hue range: 256 / 8 = 32
+      leds[map(i, 0, LED_NUMBER - 1, LED_NUMBER - 1, 0)] = CHSV((i * 32), 193, iV);
     }
     FastLED.show();  
 }
@@ -48,13 +53,14 @@ void setup() {
 
   delay(1000);
   FastLED.addLeds<LED_TYPE, LED_DATA_PIN, LED_COLOR_MODEL>(leds, LED_NUMBER).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(LED_BRIGHTNESS);
+  FastLED.setBrightness(iBrightness);
   FastLED.show();
   
   Serial.begin(9600);
   Serial.println("Start");
-  Serial.println(ENCODER_POS_PER_LED);
-
+  Serial.print("Brightness: ");
+  Serial.println(iBrightness);
+  doSetShow();
 }
 
 void loop() {
@@ -82,7 +88,6 @@ void loop() {
   }
   iValLast = iVal;
   /* END – Code for encoder */
-  int iLastLed = iEncoderPos / ENCODER_POS_PER_LED;
   if (iEncoderPos != iEncoderPosLast) {
     doSetShow();
     iEncoderPosLast = iEncoderPos;
@@ -93,10 +98,15 @@ void loop() {
     unsigned long ulThisEntry = millis();
     // 200 ms debounce test.
     if (80 < (ulThisEntry - ulLastEntry)) {
-      iHue += HUE_ORANGE;
+//      iHue += HUE_ORANGE;
+      iBrightness += LED_BRIGHTNESS_INC;
+      if ((LED_BRIGHTNESS_MIN > iBrightness) || (LED_BRIGHTNESS_MAX < iBrightness)) {
+        iBrightness = LED_BRIGHTNESS_MIN;
+      }
+      FastLED.setBrightness(iBrightness);
       doSetShow();
-      Serial.print("Hue Set: ");
-      Serial.println(iHue);
+      Serial.print("Brightness: ");
+      Serial.println(iBrightness);
     }
     ulLastEntry = ulThisEntry;
   }
